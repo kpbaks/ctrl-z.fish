083c9b778b0934058d404a0859e53a81fff5aea8
kpbaks <kristoffer.pbs@tuta.io>
2024-01-25 13:41:03 +0100

    chore: outcomment example event listeners so they dont get installed by default

--- conf.d/ctrl-z.fish
+++ conf.d/ctrl-z.fish
@@ -28,53 +28,53 @@ function __ctrl+z.fish -d "Keybind function for ctrl+z.fish. Not meant to be cal
     end
 end
 
-function __ctrl+z.fish::listener::hint_putting_in_foreground --on-event ctrl_z_to_bg -a command
-    set -l reset (set_color normal)
-    set -l blue (set_color blue)
+# function __ctrl+z.fish::listener::hint_putting_in_foreground --on-event ctrl_z_to_bg -a command
+#     set -l reset (set_color normal)
+#     set -l blue (set_color blue)
 
-    printf "\n%s><>%s press %sctrl+z%s to put the top background job (%s%s) in the foreground.\n" \
-        $blue $reset \
-        $blue $reset \
-        (printf (printf $command | fish_indent --ansi)) $reset
-end
+#     printf "\n%s><>%s press %sctrl+z%s to put the top background job (%s%s) in the foreground.\n" \
+#         $blue $reset \
+#         $blue $reset \
+#         (printf (printf $command | fish_indent --ansi)) $reset
+# end
 
-function __ctrl+z.fish::listener::cargo_check --on-event ctrl_z_to_bg -a command
-    test -f Cargo.toml; or return 0
-    cargo check
-end
+# function __ctrl+z.fish::listener::cargo_check --on-event ctrl_z_to_bg -a command
+#     test -f Cargo.toml; or return 0
+#     cargo check
+# end
 
-function __ctrl+z.fish::listener::git_status --on-event ctrl_z_to_bg -a command
-    # fg is a blocking call
-    # When it returns, it would be nice to show some contextial information
-    if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-        command git status
-    end
+# function __ctrl+z.fish::listener::git_status --on-event ctrl_z_to_bg -a command
+#     # fg is a blocking call
+#     # When it returns, it would be nice to show some contextial information
+#     if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+#         command git status
+#     end
 
-    # if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    #     set --query __ctrl_z_prev_git_status_modified
-    #     or set -g __ctrl_z_prev_git_status_modified (command git ls-files --modified)
-    #     set -l __ctrl_z_git_status_modified (command git ls-files --modified)
-    #     # Check if the two arrays are equal
-    #     set -l equal 1
-    #     # If the two arrays do not have the same length, then they are not equal
-    #     test (count $__ctrl_z_prev_git_status_modified) -ne (count $__ctrl_z_git_status_modified); and set equal 0
-    #     # If they have the same length, then check if they have the same elements
-    #     if test $equal -eq 1
-    #         for i in (seq (count $__ctrl_z_prev_git_status_modified))
-    #             if test $__ctrl_z_prev_git_status_modified[$i] != $__ctrl_z_git_status_modified[$i]
-    #                 set equal 0
-    #                 break
-    #             end
-    #         endthe
-    #     end
+#     # if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+#     #     set --query __ctrl_z_prev_git_status_modified
+#     #     or set -g __ctrl_z_prev_git_status_modified (command git ls-files --modified)
+#     #     set -l __ctrl_z_git_status_modified (command git ls-files --modified)
+#     #     # Check if the two arrays are equal
+#     #     set -l equal 1
+#     #     # If the two arrays do not have the same length, then they are not equal
+#     #     test (count $__ctrl_z_prev_git_status_modified) -ne (count $__ctrl_z_git_status_modified); and set equal 0
+#     #     # If they have the same length, then check if they have the same elements
+#     #     if test $equal -eq 1
+#     #         for i in (seq (count $__ctrl_z_prev_git_status_modified))
+#     #             if test $__ctrl_z_prev_git_status_modified[$i] != $__ctrl_z_git_status_modified[$i]
+#     #                 set equal 0
+#     #                 break
+#     #             end
+#     #         endthe
+#     #     end
 
-    #     if test $equal -eq 0
-    #         command git status
-    #         set -g __ctrl_z_prev_git_status_modified $__ctrl_z_git_status_modified
-    #     end
-    # else
-    #     ls
-    # end
-end
+#     #     if test $equal -eq 0
+#     #         command git status
+#     #         set -g __ctrl_z_prev_git_status_modified $__ctrl_z_git_status_modified
+#     #     end
+#     # else
+#     #     ls
+#     # end
+# end
 
 bind \cz '__ctrl+z.fish; commandline --function repaint'
